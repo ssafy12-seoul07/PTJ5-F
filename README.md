2931a0781a25d312ee1a88bc134f994f4309add4
seohye-ki <seohyeon.shkim@gmail.com>
2024-10-04 12:12:21 +0900

    ssafit database erd

---
+++ README.md
@@ -0,0 +1,16 @@
+# PTJ4-F
+
+> 팀장 : 최혁규  
+> 팀원 : 김서현, 배장한
+
+## 요구사항
+Ssafit Database 구현
+
+### 기본 기능
+- Database ERD작성
+- DDL작성 후 DB 생성
+
+## 구현 방식
+- Mysql EER Diagram사용하여 구조를 작성한 후, 생성
+
+## 발생한 이슈
---
+++ resource/schema.sql
@@ -0,0 +1,57 @@
+-- -----------------------------------------------------
+-- Schema pjt5_f_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pjt5_f_db` DEFAULT CHARACTER SET utf8 ;
+USE `pjt5_f_db` ;
+
+-- -----------------------------------------------------
+-- Table `pjt5_f_db`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pjt5_f_db`.`User` (
+  `user_id` VARCHAR(15) NOT NULL,
+  `user_nickname` VARCHAR(24) NOT NULL,
+  `user_password` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `user_nickname_UNIQUE` (`user_nickname` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pjt5_f_db`.`Video`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pjt5_f_db`.`Video` (
+  `video_id` INT NOT NULL AUTO_INCREMENT,
+  `video_title` VARCHAR(45) NOT NULL,
+  `video_part` VARCHAR(15) NOT NULL,
+  `video_channel_name` VARCHAR(45) NOT NULL,
+  `video_url` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`video_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pjt5_f_db`.`Review`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pjt5_f_db`.`Review` (
+  `review_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(15) NULL,
+  `video_id` INT NULL,
+  `review_title` VARCHAR(45) NULL,
+  `review_content` VARCHAR(200) NULL,
+  `review_created_at` DATETIME NULL DEFAULT now(),
+  `review_like_cnt` INT NULL DEFAULT 0,
+  `review_dislike_cnt` INT NULL DEFAULT 0,
+  PRIMARY KEY (`review_id`),
+  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
+  INDEX `video_id_idx` (`video_id` ASC) VISIBLE,
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `pjt5_f_db`.`User` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `video_id`
+    FOREIGN KEY (`video_id`)
+    REFERENCES `pjt5_f_db`.`Video` (`video_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
